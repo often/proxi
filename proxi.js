e578482d18d8fc47d3c54734c15bc3c73f0b6046
Sany <44934721+often@users.noreply.github.com>
2021-08-07 14:08:30 -0500

    major improvements
    works much better and is more flexible than the last version.
    now with raw and JSON response support!

--- proxi.js
+++ proxi.js
@@ -1,24 +1,60 @@
 let handle = async request =>
 {
   let url = new URL(request.url)
-  let responseHeaders = new Headers({
-    'content-type': 'text/plain; charset=utf-8',
-    'access-control-allow-origin': '*'
+  let response = {}
+  let pretty = url.searchParams.has('pretty')
+  let raw = url.searchParams.has('raw')
+
+  try
+  {
+    url = new URL(url.searchParams.get('url'))
+    let {method, headers, body} = request
+    let res = await fetch(url, {method, headers, body})
+    let responseHeaders = {}
+
+    for (let [name, value] of res.headers)
+      responseHeaders[name] = value
+
+    if (raw)
+    {
+      responseHeaders['access-control-allow-origin'] = '*'
+
+      return new Response(res.body, {
+        status: res.status,
+        headers: responseHeaders
+      })
+    }
+    else
+    {
+      response.success = true
+      response.url = res.url
+      response.ok = res.ok
+      response.redirected = res.redirected
+      response.status = res.status
+      response.statusText = res.statusText
+      response.headers = responseHeaders
+      response.body = await res.text()
+    }
+  }
+  catch (error)
+  {
+    response.success = false
+    response.message = error.message
+  }
+
+  if (pretty)
+    response = JSON.stringify(response, null, 2)
+  else
+    response = JSON.stringify(response)
+
+  return new Response(response, {
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+      'access-control-allow-origin': '*'
+    }
   })
-
-  try { url = new URL(url.search.slice(1)) }
-  catch { return new Response('missing or invalid url', { status: 400, headers: responseHeaders }) }
-
-  let response
-  let { method, headers, body } = request
-
-  try { response = await fetch(url, { method, headers, body }) }
-  catch (error) { return new Response(error.message, { status: 502, headers: responseHeaders }) }
-
-  for (let [name, value] of response.headers)
-  if (name != 'access-control-allow-origin') responseHeaders.set(name, value)
-
-  return new Response(response.body, { status: response.status, headers: responseHeaders })
 }
 
-addEventListener('fetch', event => event.respondWith(handle(event.request)))
+addEventListener('fetch', event => {
+  event.respondWith(handle(event.request))
+})
